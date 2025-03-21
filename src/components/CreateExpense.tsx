4bfe16c84960a5a373bca9d7cdc3479cf11e2f0d
hj <hyeonjubaek06@gmail.com>
2025-03-22 06:05:44 +0900

    add: CreateExpense 컴포넌트에 입력 필드 추가

--- src/components/CreateExpense.tsx
+++ src/components/CreateExpense.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const CreateExpense = () => {
-  const id
-    return (
+  return (
     <div>
-        
-    </div>
-  )
-}
+      <div>
+        <p>날짜</p>
+        <input className="" type="date" />
+      </div>
 
-export default CreateExpense
+      <div>
+        <p>항목</p>
+        <input className="" type="text" />
+      </div>
+
+      <div>
+        <p>금액</p>
+        <input className="" type="number" />
+      </div>
+
+      <div>
+        <p>내용</p>
+        <input className="" type="text" />
+      </div>
+    </div>
+  );
+};
+
+export default CreateExpense;
