b68b37997b48369ef8580cc0559acc549c3dcaf8
hj <hyeonjubaek06@gmail.com>
2025-03-22 06:07:31 +0900

    add: ExpenseList 컴포넌트에 수정 및 삭제 버튼 추가

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,9 +1,14 @@
-
 const ExpenseList = () => {
-
   return (
-    <div>ExpenseList</div>
-  )
-}
+    <div>
+      <ul>
+        <li>
+          <button>수정</button>
+          <button>삭제</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default ExpenseList
+export default ExpenseList;
