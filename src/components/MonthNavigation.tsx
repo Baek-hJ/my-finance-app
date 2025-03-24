4d2717b96b5954480f86eb31796026c5a83ca4f7
hj <hyeonjubaek06@gmail.com>
2025-03-25 02:57:34 +0900

    style: MonthNavigation 및 Home 컴포넌트의 스타일 수정

--- src/components/MonthNavigation.tsx
+++ src/components/MonthNavigation.tsx
@@ -4,9 +4,9 @@ const MonthNavigation = () => {
 const buttons = Array.from({length: 12}, (_, i) => i + 1)
 
     return (
-    <div className="flex flex-row bg-white p-4 rounded-lg shadow-md items-center justify-center gap-4 md:gap-6 bor">
+    <div className="flex flex-row bg-white p-4 rounded-lg shadow-md items-center justify-center gap-4 md:gap-6 grid grid-cols-6">
         {buttons.map((month) => (
-        <button key={month}>{month}월</button>
+        <button key={month} className="border-2 md:border-solid w-20 ">{month}월</button>
         ))}
     </div>
   )
