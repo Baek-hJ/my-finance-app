ca2fa772181e668839069912a6a68daf61367077
hj <hyeonjubaek06@gmail.com>
2025-03-25 02:39:56 +0900

    fix: CreateExpense 컴포넌트의 입력 필드에 플레이스홀더 추가

--- src/components/CreateExpense.tsx
+++ src/components/CreateExpense.tsx
@@ -4,22 +4,22 @@ const CreateExpense = () => {
     <form className="flex flex-row bg-white p-4 rounded-lg shadow-md">
       <div className="basis-auto">
         <p>날짜</p>
-        <input className="" type="date" />
+        <input className="" type="text" placeholder="YYYY-MM-DD"/>
       </div>
 
       <div className="basis-auto">
         <p>항목</p>
-        <input className="" type="text" />
+        <input className="" type="text" placeholder="지출 항목" />
       </div>
 
       <div className="basis-auto">
         <p>금액</p>
-        <input className="" type="number" />
+        <input className="" type="number" placeholder="지출 금액"/>
       </div>
 
       <div className="basis-auto">
         <p>내용</p>
-        <input className="" type="text" />
+        <input className="" type="text" placeholder="지출 내용" />
       </div>
 
       <button className="basis-auto">저장</button>
