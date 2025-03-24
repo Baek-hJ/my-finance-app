41be350690a0c777e10b252ecee1ddcbb4e2de40
hj <hyeonjubaek06@gmail.com>
2025-03-25 03:31:41 +0900

    style: CreateExpense 컴포넌트의 입력 필드에 스타일 및 플레이스홀더 크기 조정

--- src/components/CreateExpense.tsx
+++ src/components/CreateExpense.tsx
@@ -1,25 +1,25 @@
 
 const CreateExpense = () => {
   return (
-    <form className="flex flex-row bg-white p-4 rounded-xl shadow-md">
+    <form className="flex flex-row bg-white p-4 gap-10 rounded-xl shadow-md">
       <div className="basis-auto">
         <p>날짜</p>
-        <input className="" type="text" placeholder="YYYY-MM-DD"/>
+        <input className="w-20 placeholder:text-[12px]" type="text" placeholder="YYYY-MM-DD"/>
       </div>
 
       <div className="basis-auto">
         <p>항목</p>
-        <input className="" type="text" placeholder="지출 항목" />
+        <input className="w-20 placeholder:text-[12px]" type="text" placeholder="지출 항목" />
       </div>
 
       <div className="basis-auto">
         <p>금액</p>
-        <input className="" type="number" placeholder="지출 금액"/>
+        <input className="w-20 placeholder:text-[12px]" type="number" placeholder="지출 금액"/>
       </div>
 
       <div className="basis-auto">
         <p>내용</p>
-        <input className="" type="text" placeholder="지출 내용" />
+        <input className="w-20 placeholder:text-[12px]" type="text" placeholder="지출 내용" />
       </div>
 
       <button className="basis-auto">저장</button>
