705705cdea7fdcf450f0076b21e65add470a7f6d
hj <hyeonjubaek06@gmail.com>
2025-03-25 02:35:44 +0900

    fix: Home 페이지의 Todo List 제목에서 불필요한 클래스 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import MonthNavigation from "../components/MonthNavigation";
 const Home = () => {
   return (
     <div className="w-screen h-screen bg-gradient-to-br from-[#0f0f0f] via-[#1a1a2e] to-[#3c1053] bg-[length:200%_200%] bg-[position:0%_50%] animate-gradient-move flex flex-col items-center justify-center space-y-4">
-    <h1 className="font-sans md:font-sans text-[4rem] text-start font-semibold text-white/60 shadow-xl">Todo List</h1>
+    <h1 className="font-sans md:font-sans text-[4rem] text-start font-semibold text-white/60 ">Todo List</h1>
     <div className="grid gap-4 justify-center md:object-top">
       <MonthNavigation />
       <CreateExpense />
