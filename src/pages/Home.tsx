e5dbaa787cb8ed69069f3c789c03c4d34f17c599
hj <hyeonjubaek06@gmail.com>
2025-03-26 02:27:53 +0900

    feat: supabase에서 데이터 가져오는 로직 개선 및 선택된 월에 따라 렌더링 업데이트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,20 +19,29 @@ const Home = () => {
       // 다음 달의 1일까지
       const endMonth = selectedMonth === 12 ? 1 : selectedMonth + 1;
       const endYear = selectedMonth === 12 ? year + 1 : year;
-      const { data, error } = await supabase.from("expenses").select("*");
+      const endDate = `${endYear}${String(endMonth).padStart(2,"0")}-01`;
+
+      // supabase에서 데이터 가져오기
+      const { data, error } = await supabase
+      .from("expenses")
+      .select("*")
+      .gte("data", startDate)
+      .lte("column", endDate)
+      .order("date", {ascending: false}); // 날짜 내림차순순
 
       if (error) {
         console.error("Error fetching expenses:", error.message);
         return;
       }
 
+      // 데이터가 있으면 화면에 렌더링을 해주기 위해 저장
       if (data) {
         setExpenses(data);
       }
     };
 
     fetchExpenses();
-  }, []);
+  }, [selectedMonth]);
 
   return (
     <div className="min-w-screen min-h-screen md:w-screen md:h-screen bg-gradient-to-br from-[#0f0f0f] via-[#1a1a2e] to-[#3c1053] bg-[length:200%_200%] bg-[position:0%_50%] animate-gradient-move flex flex-col items-center justify-center space-y-4">
