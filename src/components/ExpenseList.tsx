3fc8c2d5b3e328cddad6ac2a5dffadbc5ca6e5f3
hj <hyeonjubaek06@gmail.com>
2025-03-26 15:26:33 +0900

    feat: ExpenseList 컴포넌트에서 지출 항목을 목록으로 표시하도록 수정

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -4,13 +4,23 @@ interface ExpenseListProps {
   expenses: Expense[];
 }
 
-const ExpenseList = ({expenses}:ExpenseListProps) => {
+const ExpenseList = ({ expenses }: ExpenseListProps) => {
   return (
     <div className="flex flex-row bg-white p-4 rounded-xl shadow-md">
-      <ul>
-        <li>
-
-        </li>
+      <ul className="divide-y divide-gray-200">
+        {expenses.map((expense) => (
+          <li key={expense.id}>
+            <div className="flex justify-between items-center">
+              <span className="font-medium">
+                {expense.item} - {expense.description}
+              </span>
+              <span className="text-sm text-gray-500">{expense.date}</span>
+              <span className="font-semibold text-indigo-700">
+                {expense.amount}원
+              </span>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
