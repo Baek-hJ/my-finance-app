f32b5d71ecb28218eb1a8af1b1ac6dd50ecf4380
hj <hyeonjubaek06@gmail.com>
2025-03-26 14:26:05 +0900

    feat: Home 컴포넌트의 제목 크기 조정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -45,7 +45,7 @@ const Home = () => {
 
   return (
     <div className="min-w-screen min-h-screen md:w-screen md:h-screen bg-gradient-to-br from-[#0f0f0f] via-[#1a1a2e] to-[#3c1053] bg-[length:200%_200%] bg-[position:0%_50%] animate-gradient-move flex flex-col items-center justify-center space-y-4">
-      <h1 className="font-sans md:font-sans text-[4rem] text-start font-semibold text-white/60 ">
+      <h1 className="font-sans md:font-sans text-[3.5rem] text-start font-semibold text-white/60 ">
         My Financial Management
       </h1>
 
