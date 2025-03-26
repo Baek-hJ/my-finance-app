18e5b7ac01e7b3c8cf61a5d35170ed351c56a948
hj <hyeonjubaek06@gmail.com>
2025-03-26 15:06:45 +0900

    feat: MonthNavigation 컴포넌트에 선택된 월 상태 관리 기능 추가

--- src/components/MonthNavigation.tsx
+++ src/components/MonthNavigation.tsx
@@ -1,15 +1,37 @@
-
-
-const MonthNavigation = () => {
-const buttons = Array.from({length: 12}, (_, i) => i + 1)
-
-    return (
-    <div className="grid grid-cols-3 md:grid-cols-6 gap-4 bg-white p-4 rounded-xl shadow-md">
-        {buttons.map((month) => (
-        <button key={month} className="bg-slate-100 hover:bg-indigo-200 hover:border-2 border-indigo-300 hover:cursor-pointer w-full h-12 text-center rounded-lg">{month}월</button>
-        ))}
-    </div>
-  )
+interface MonthNavigationProps {
+  selectedMonth: number;
+  setSelectedMonth: (mount: number) => void;
 }
 
-export default MonthNavigation
+const MonthNavigation = ({
+  selectedMonth,
+  setSelectedMonth,
+}: MonthNavigationProps) => {
+  const buttons = Array.from({ length: 12 }, (_, i) => i + 1);
+
+  return (
+    <div className="grid grid-cols-3 md:grid-cols-6 gap-4 bg-white p-4 rounded-xl shadow-md">
+      {buttons.map((month) => {
+        const isSelected = selectedMonth === month;
+        return (
+          <button
+            key={month}
+            onClick={() => setSelectedMonth(month)}
+            className={`bg-slate-100 hover:bg-indigo-200 hover:border-2 border-indigo-300 hover:cursor-pointer w-full h-12 text-center rounded-lg
+              ${
+                isSelected
+                  ? "bg-indigo-500 text-white border-2 border-indigo-700"
+                  : "bg-slate-100 hover:bg-indigo-200 hover:border-2 border-indigo-300"
+              }
+              hover:cursor-pointer transition
+              `}
+          >
+            {month}월
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MonthNavigation;
