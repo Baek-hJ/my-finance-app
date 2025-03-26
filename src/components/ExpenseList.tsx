1c9acec33abc63af27b27aecfd9cf4c45ae339c2
hj <hyeonjubaek06@gmail.com>
2025-03-26 15:11:02 +0900

    feat: ExpenseList 컴포넌트에 props를 추가하여 지출 항목을 전달하도록 수정

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,10 +1,15 @@
-const ExpenseList = () => {
+import { Expense } from "../../database.types";
+
+interface ExpenseListProps {
+  expenses: Expense[];
+}
+
+const ExpenseList = ({expenses}:ExpenseListProps) => {
   return (
     <div className="flex flex-row bg-white p-4 rounded-xl shadow-md">
       <ul>
         <li>
-          <button>수정</button>
-          <button>삭제</button>
+
         </li>
       </ul>
     </div>
