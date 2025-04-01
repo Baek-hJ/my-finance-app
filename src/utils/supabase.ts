dd33bb65e110b665d13e7db314493468f9c84179
hj <hyeonjubaek06@gmail.com>
2025-04-01 15:13:20 +0900

    add: 변수로 Supabase 클라이언트 초기화 URL 및 익명 키 설정

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Create a single supabase client for interacting with your database
-export const supabase = createClient(
-  "https://xyzcompany.supabase.co",
-  "public-anon-key"
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
