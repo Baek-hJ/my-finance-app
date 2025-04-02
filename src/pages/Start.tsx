12812ecf250879617f79f8b01d98d941d3b12a51
hj <hyeonjubaek06@gmail.com>
2025-04-02 15:10:35 +0900

    update: Start 페이지의 이미지 클릭 시 Home으로 네비게이션 기능 개선

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -5,7 +5,12 @@ const Start = () => {
 
   return (
     <div className="min-w-screen min-h-screen md:w-screen md:h-screen bg-[#DBE9E9] flex flex-col items-center justify-center">
-      <img src="/folder.png" alt="Folder" className="h-[13rem] cursor-pointer m-[1rem]" onClick={() => navigate('/Home')} />
+      <img
+        src="/folder.png"
+        alt="Folder"
+        className="h-[13rem] cursor-pointer m-[1rem]"
+        onClick={() => navigate("/Home")}
+      />
       <h1 className="text-5xl ">my-finance-app</h1>
     </div>
   );
