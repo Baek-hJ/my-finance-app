f3aa88d7c5e8efb5fb5781cc20d68826f537654b
hj <hyeonjubaek06@gmail.com>
2025-04-02 15:45:13 +0900

    update: Home 컴포넌트에 스타일 추가하여 화면 크기 및 배경색 설정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import ExpenseList from "../components/ExpenseList";
 import MonthNavigation from "../components/MonthNavigation";
 
 const Home = () => {
-  return <div>
+  return <div className="min-w-screen min-h-screen md:w-screen md:h-screen bg-[#DBE9E9] flex flex-col items-center justify-center">
     <MonthNavigation/>
     <CreateExpense/>
     <ExpenseList/>
