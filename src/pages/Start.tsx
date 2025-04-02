3fc901c68a952c96d5126921eaec4f74171e1e64
hj <hyeonjubaek06@gmail.com>
2025-04-02 13:01:00 +0900

    add: stars 페이지에에 스타일 추가 및 이미지 삽입

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,5 +1,10 @@
 const Start = () => {
-  return <div>Start</div>;
+  return (
+    <div className="min-w-screen min-h-screen md:w-screen md:h-screen bg-[#DBE9E9]  flex flex-col items-center justify-center">
+      <img src="/folder.png" alt="Folder" className="h-[22rem] " />
+      <h1 className="text-5xl">my-finance-app</h1>
+    </div>
+  );
 };
 
 export default Start;
