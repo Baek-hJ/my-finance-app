d045b80057bbe2fad394718307e2c3bbaf0f29dc
hj <hyeonjubaek06@gmail.com>
2025-04-03 15:21:25 +0900

    update: 홈 페이지 레이아웃 개선 및 스타일 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,22 @@ import ExpenseList from "../components/ExpenseList";
 import MonthNavigation from "../components/MonthNavigation";
 
 const Home = () => {
-  return <div className="min-w-screen min-h-screen md:w-screen md:h-screen bg-[#DBE9E9] flex flex-col items-center justify-center">
-    <MonthNavigation/>
-    <CreateExpense/>
-    <ExpenseList/>
-  </div>;
+  return (
+    <div className="min-w-screen min-h-screen md:w-screen md:h-screen bg-[#DBE9E9] flex flex-col items-center justify-center">
+      <div className=" p-[1rem] border-2 bg-[#CFDCDC] size-40 w-[80%] md:w-[70%] md:h-[80%]">
+      <h1 className="font-semibold">my-finace-app/3</h1>
+        {/* <div className="border size-32 md:h-[70%] md:w-[99%] bg-white"> */}
+        <div>
+        <MonthNavigation />
+        </div>
+        <div className="p-[1rem] border size-32 md:h-[70%] md:w-[99%] bg-white overflow-y-scroll border ">
+        <ExpenseList />
+        </div>
+        {/* </div> */}
+        <CreateExpense />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
