5c8daf9a0f4101ae06bbd76da2f1fa68b92d350c
hj <hyeonjubaek06@gmail.com>
2025-04-04 12:04:35 +0900

    update: CreateExpense 컴포넌트의 레이아웃 개선 및 버튼 추가

--- src/components/CreateExpense.tsx
+++ src/components/CreateExpense.tsx
@@ -1,41 +1,33 @@
 const CreateExpense = () => {
   return (
-    <div className="grid grid-cols-2 gap-4 p-[1.5%]">
-      <div className="flex flex-wrap justify-start gap-7">
-        <h1>날짜</h1>
-        <input
-          className="border bg-white w-[15rem] h-[1.5rem] placeholder:p-4 placeholder:text-[15px] placeholder: "
-          placeholder="YYYY-MM-DD"
-          type="text"
-        />
-      </div>
-      <div className="flex flex-wrap justify-start gap-7">
-        {" "}
-        <h1>항목</h1>
-        <input
-          className="border bg-white w-[15rem] h-[1.5rem] placeholder:p-4 placeholder:text-[15px] placeholder: "
-          placeholder="지출 항목"
-          type="text"
-        />
-      </div>
-      <div className="flex flex-wrap justify-start gap-7">
-        {" "}
-        <h1>가격</h1>
-        <input
-          className="border bg-white w-[15rem] h-[1.5rem] placeholder:p-4 placeholder:text-[15px] placeholder: "
-          placeholder="0"
-          type="number"
-        />
-      </div>
-      <div className="flex flex-wrap justify-start gap-7">
-        {" "}
-        <h1>내용</h1>
-        <input
-          className="border bg-white w-[15rem] h-[1.5rem] placeholder:p-4 placeholder:text-[15px] placeholder: "
-          placeholder="지출 내용"
-          type="text"
-        />
-      </div>
+    <div className="grid grid-cols-[auto_1fr_auto_1fr_auto] gap-2 items-center p-[2%]">
+      <h1 className="text-right ">날짜</h1>
+      <input
+        className="border bg-white w-[15rem] h-[1.7rem] placeholder:p-2 placeholder:text-[15px]"
+        placeholder="YYYY-MM-DD"
+        type="text"
+      />
+      <h1 className="text-right">가격</h1>
+      <input
+        className="border bg-white w-[15rem] h-[1.7rem] placeholder:p-2 placeholder:text-[15px]"
+        placeholder="0"
+        type="number"
+      />
+      <button className="border p-2 w-[5rem] h-[1.7rem]">저장</button>
+
+      <h1 className="text-right">항목</h1>
+      <input
+        className="border bg-white w-[15rem] h-[1.7rem] placeholder:p-2 placeholder:text-[15px]"
+        placeholder="지출 항목"
+        type="text"
+      />
+      <h1 className="text-right">내용</h1>
+      <input
+        className="border bg-white w-[15rem] h-[1.7rem] placeholder:p-2 placeholder:text-[15px]"
+        placeholder="지출 내용"
+        type="text"
+      />
+      <button className="border p-2 w-[5rem] h-[1.7rem]">취소</button>
     </div>
   );
 };
