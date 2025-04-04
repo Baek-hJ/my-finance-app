0a8d451dd9bcd74873786609eb96be01f6a9bdbb
hj <hyeonjubaek06@gmail.com>
2025-04-04 11:57:51 +0900

    update: Home 컴포넌트의 레이아웃 수정 및 높이 조정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,9 @@ import MonthNavigation from "../components/MonthNavigation";
 const Home = () => {
   return (
     <div className="min-w-screen min-h-screen md:w-screen md:h-screen bg-[#DBE9E9] flex flex-col items-center justify-center">
-      <div className=" p-[1rem] border-2 bg-[#CFDCDC] size-40 w-[80%] md:w-[70%] md:h-[80%]">
-        
+      <div className=" p-[1rem] border-2 bg-[#CFDCDC] size-40 w-[80%] md:w-[70%] md:h-[85%]">
         <h1 className="font-semibold">my-finace-app/</h1>
-        
+
         <div className="bg-[#F1F1F1] border md:h-[2.3rem] md:w-[99%]">
           <MonthNavigation />
         </div>
