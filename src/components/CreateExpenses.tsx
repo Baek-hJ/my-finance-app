92b8349cbdcf10807e0cd43d2454a61dfe276e5c
hj <hyeonjubaek06@gmail.com>
2025-04-10 12:13:49 +0900

    update: CreateExpenses 컴포넌트의 코드 스타일 개선 및 불필요한 코드 제거

--- src/components/CreateExpenses.tsx
+++ src/components/CreateExpenses.tsx
@@ -8,8 +8,9 @@ const CreateExpenses = () => {
   const [addDescription, setAddDescription] = useState<string | null>("");
 
   const handleChange = async () => {
-    const {error} = await supabase.from("expenses").insert([
+    const { error } = await supabase.from("expenses").insert([
       {
+        id: crypto.randomUUID(),
         date: addDate,
         amount: addAmount,
         item: addItem,
@@ -19,17 +20,27 @@ const CreateExpenses = () => {
     if (error) {
       console.error("Error inserting data:", error);
     }
-  }
+  };
   return (
-    <form className="grid grid-cols-[auto_1fr_auto_1fr_auto] gap-3 items-center p-5">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleChange();
+      }}
+      className="grid grid-cols-[auto_1fr_auto_1fr_auto] gap-3 items-center p-5"
+    >
       <h1 className="text-right ">날짜</h1>
       <input
+        value={addDate}
+        onChange={(e) => setAddDate(e.target.value)}
         className="border bg-white w-[15rem] h-[1.7rem] placeholder:p-2 placeholder:text-[15px]"
         placeholder="YYYY-MM-DD"
         type="text"
       />
       <h1 className="text-right">가격</h1>
       <input
+        value={addAmount ?? ""}
+        onChange={(e) => setAddAmount(Number(e.target.value))}
         className="border bg-white w-[15rem] h-[1.7rem] placeholder:p-2 placeholder:text-[15px]"
         placeholder="0"
         type="number"
@@ -40,12 +51,16 @@ const CreateExpenses = () => {
 
       <h1 className="text-right">항목</h1>
       <input
+        value={addItem ?? ""}
+        onChange={(e) => setAddItem(e.target.value)}
         className="border bg-white w-[15rem] h-[1.7rem] placeholder:p-2 placeholder:text-[15px]"
         placeholder="지출 항목"
         type="text"
       />
       <h1 className="text-right">내용</h1>
       <input
+        value={addDescription ?? ""}
+        onChange={(e) => setAddDescription(e.target.value)}
         className="border bg-white w-[15rem] h-[1.7rem] placeholder:p-2 placeholder:text-[15px]"
         placeholder="지출 내용"
         type="text"
