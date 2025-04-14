62042d77fa81863e7dd08f53005b4696a9ff17d2
hj <hyeonjubaek06@gmail.com>
2025-04-14 19:23:06 +0900

    fix: CreateExpense 컴포넌트 이름을 CreateExpenses로 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -73,7 +73,7 @@ const Home = () => {
           <ExpensesList expenses={expenses} selectedMonth={selectedMonth}/>
         </div>
 
-        <CreateExpense />
+        <CreateExpenses />
       </div>
     </div>
   );
