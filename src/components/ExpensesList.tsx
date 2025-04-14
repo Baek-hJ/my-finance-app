9af2179c3a2c784364f148969843139e9ab62182
hj <hyeonjubaek06@gmail.com>
2025-04-14 17:42:29 +0900

    update: ExpensesList 컴포넌트에서 월별 필터링 기능 추가 및 상태 관리 개선

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -1,34 +1,27 @@
-import { useEffect, useState } from "react";
-import { supabase } from "../utils/supabase";
 import { Expense } from "../../database.types";
+import getMonthFilter from "../utils/getMonthFilter";
 
-const ExpensesList = () => {
-  const [expenses, setExpenses] = useState<Expense[]>([]);
-
-  useEffect(() => {
-    const fetchExpenses = async () => {
-      const { data, error } = await supabase.from("expenses").select("*");
-
-      if (error) {
-        console.error("Error fetching expenses:", error);
-      } else {
-        setExpenses(data);
-      }
-    };
-
-    fetchExpenses();
-  }, []);
+const ExpensesList = ({
+  expenses,
+  selectedMonth,
+}: {
+  expenses: Expense[];
+  selectedMonth: number;
+}) => {
+  const filteredExpenses = getMonthFilter(expenses, selectedMonth);
   return (
     <div>
       <ul className="list-disc list-outside pl-6">
-        {expenses.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <li key={expense.id}>
             <div className="grid grid-cols-[7rem_6rem_5rem] gap-x-4">
               <p>{expense.date}</p>
               <p className="text-mbold font-bold">
                 {expense.item} - {expense.description}
               </p>
-              <p className="text-mbold font-bold text-[#179494]">{expense.amount}원</p>
+              <p className="text-mbold font-bold text-[#179494]">
+                {expense.amount}원
+              </p>
             </div>
           </li>
         ))}
