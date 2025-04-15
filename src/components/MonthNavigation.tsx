a51c48da27cb65a28bae76a13d4b0bab977dadad
hj <hyeonjubaek06@gmail.com>
2025-04-15 19:06:17 +0900

    update: MonthNavigation 컴포넌트에서 버튼 클릭 핸들러 추가

--- src/components/MonthNavigation.tsx
+++ src/components/MonthNavigation.tsx
@@ -3,7 +3,7 @@ const MonthNavigation:React.FC = () => {
     <div className="flex flex-wrap justify-start gap-4 p-2">
       <p>Month</p>
       {Array.from({ length : 12}, (_,i) => ( 
-      <button key={i}>
+      <button key={i} onClick={}>
         {i+1}
       </button>
     ))}
