157f05ccd4195a51305cac3a461223a1c8cbadfe
hj <hyeonjubaek06@gmail.com>
2025-04-18 18:09:52 +0900

    style: ExpensesList 컴포넌트의 항목에 마진 추가 및 패딩 수정하여 레이아웃 개선

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -11,10 +11,10 @@ const ExpensesList = ({
   const filteredExpenses = getMonthFilter(expenses, selectedMonth);
   return (
     <div>
-      <ul className="list-disc list-outside pl-6">
+      <ul className="list-disc list-outside ml-10">
         {filteredExpenses.map((expense) => (
           <li key={expense.id}>
-            <div className="flex items-center gap-3 text-xl">
+            <div className="m-5 flex items-center gap-3 text-xl">
               <p className="mr-5 shrink-0">{expense.date}</p>
               <p className="mr-5 shrink-0 text-mbold font-bold">{expense.item} - {expense.description}</p>
               <p className="shrink-0 text-mbold font-bold text-[#179494]">{expense.amount}원</p>
