bdc8a4c8265109265cf37293f71f1dd76c0a0cc8
hj <hyeonjubaek06@gmail.com>
2025-04-18 18:07:47 +0900

    update: ExpensesList 컴포넌트의 항목 레이아웃을 그리드에서 플렉스 스타일로 변경

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -14,16 +14,10 @@ const ExpensesList = ({
       <ul className="list-disc list-outside pl-6">
         {filteredExpenses.map((expense) => (
           <li key={expense.id}>
-            <div className="grid grid-cols-[7rem_1fr_5rem] gap-x-4 items-center">
-              <p>{expense.date}</p>
-              <div className="overflow-hidden">
-                <p className="text-mbold font-bold truncate">
-                  {expense.item} - {expense.description}
-                </p>
-              </div>
-              <p className="text-mbold font-bold text-[#179494]">
-                {expense.amount}원
-              </p>
+            <div className="flex items-center gap-3 text-xl">
+              <p className="shrink-0">{expense.date}</p>
+              <p className="shrink-0 text-mbold font-bold">{expense.item} - {expense.description}</p>
+              <p className="shrink-0 text-mbold font-bold text-[#179494]">{expense.amount}원</p>
             </div>
           </li>
         ))}
