4dd08e90d7f112d3fa554e9140f907db392a2a0b
hj <hyeonjubaek06@gmail.com>
2025-04-18 17:54:53 +0900

    update: ExpensesList 컴포넌트의 레이아웃 개선 및 항목 설명의 가독성 향상

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -14,11 +14,13 @@ const ExpensesList = ({
       <ul className="list-disc list-outside pl-6">
         {filteredExpenses.map((expense) => (
           <li key={expense.id}>
-            <div className="grid grid-cols-[7rem_6rem_5rem] gap-x-4">
+            <div className="grid grid-cols-[7rem_1fr_5rem] gap-x-4 items-center">
               <p>{expense.date}</p>
-              <p className="text-mbold font-bold">
-                {expense.item} - {expense.description}
-              </p>
+              <div className="overflow-hidden">
+                <p className="text-mbold font-bold truncate">
+                  {expense.item} - {expense.description}
+                </p>
+              </div>
               <p className="text-mbold font-bold text-[#179494]">
                 {expense.amount}원
               </p>
