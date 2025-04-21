97a527c8f21b8a784b8b6f9e9d9c735cdc64d4f6
hj <hyeonjubaek06@gmail.com>
2025-04-21 16:57:44 +0900

    fix: ExpensesList 컴포넌트에서 내비게이션 함수 수정

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Expense } from "../../database.types";
 import getMonthFilter from "../utils/getMonthFilter";
 
@@ -9,12 +9,13 @@ const ExpensesList = ({
   expenses: Expense[];
   selectedMonth: number;
 }) => {
+  const navigate = useNavigate();
   const filteredExpenses = getMonthFilter(expenses, selectedMonth);
   return (
     <div>
       <ul className="text-xl list-disc list-outside pl-10 grid gap-5" >
         {filteredExpenses.map((expense) => (
-          <li key={expense.id} onClick={() => Navigate(`/Detail/${expense.id}`)} className="cursor-pointer hover:bg-[#A4C8C8] p-2 rounded-md">
+          <li key={expense.id} onClick={() => navigate(`/expenses/${expense.id}`)} className="cursor-pointer hover:bg-[#A4C8C8] p-2 rounded-md">
             <div className="grid grid-cols-[9rem_15rem_10rem] gap-10">
               <p>{expense.date}</p>
               <p className="text-mbold font-bold">
