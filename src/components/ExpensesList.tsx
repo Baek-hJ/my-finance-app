6d3a6e259bb9d36cd8dfc4dcca40cfee3fa6c33b
hj <hyeonjubaek06@gmail.com>
2025-04-22 17:37:41 +0900

    fix: ExpensesList 컴포넌트에서 불필요한 지출 목록 렌더링 코드 제거

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -13,21 +13,6 @@ const ExpensesList = ({
   const filteredExpenses = getMonthFilter(expenses, selectedMonth);
   return (
     <div>
-      <ul className="text-xl list-disc list-outside pl-10 grid gap-5" >
-        {filteredExpenses.map((expense) => (
-          <li key={expense.id} onClick={() => navigate(`/expenses/${expense.id}`)} className="cursor-pointer hover:bg-[#A4C8C8] p-2 rounded-md">
-            <div className="grid grid-cols-[9rem_15rem_10rem] gap-10">
-              <p>{expense.date}</p>
-              <p className="text-mbold font-bold">
-                {expense.item} - {expense.description}
-              </p>
-              <p className="text-mbold font-bold text-[#179494]">
-                {expense.amount}원
-              </p>
-            </div>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
