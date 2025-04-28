c0150db426784daeeb90a339a74a5d10de2e906a
hj <hyeonjubaek06@gmail.com>
2025-04-28 20:23:47 +0900

    fix: Start 컴포넌트의 오디오 재생 로직 수정 및 볼륨 설정 조정

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -5,10 +5,10 @@ const Start = () => {
   const audio = new Audio("audio.mp3");
 
   audio.play();
-  audio.pause()
+  audio.pause();
   audio.loop = true;
-  audio.src = "public/Strawberry.mp3"
-  audio.volume = 0.5
+  audio.src = "public/Strawberry.mp3";
+  audio.volume = 0.3;
 
   return (
     <div className="min-w-screen min-h-screen md:w-screen md:h-screen bg-[#DBE9E9] flex flex-col items-center justify-center">
@@ -16,7 +16,7 @@ const Start = () => {
         src="/folder.png"
         alt="Folder"
         className="h-[13rem] cursor-pointer m-[1rem]"
-        onClick={() => navigate("/home")}
+        onClick={() => {navigate("/home"); audio.play()}}
       />
       <h1 className="text-5xl font-sans font-bold">my-finance-app</h1>
     </div>
