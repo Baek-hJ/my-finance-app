bf34623bae1e81e172c206d3eb4db49df2e3a860
hj <hyeonjubaek06@gmail.com>
2025-04-28 20:59:27 +0900

    feat: Layout 컴포넌트에 이미지 추가 및 구조 수정

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div>
+      <img
+        width="26"
+        height="26"
+        src="https://img.icons8.com/metro/52/speaker.png"
+        alt="speaker"
+        className="color-[#CFDCDC]"
+      />{" "}
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
